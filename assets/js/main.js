9e44f494766dae348af2ce57e41230f288b445b8
Finlay Goodwin Campbell <finlaycampbell5@icloud.com>
2024-09-07 17:40:05 +1000

    Updates to Links

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,19 +98,22 @@
 		// Lightbox gallery.
 			$window.on('load', function() {
 
-				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
-					overlayColor: '#2c2c2c',
-					overlayOpacity: 0.85,
-					popupCloserText: '',
-					popupLoaderText: '',
-					selector: '.work-item a.image',
-					usePopupCaption: true,
-					usePopupDefaultStyling: false,
-					usePopupEasyClose: false,
-					usePopupNav: true,
-					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-				});
+				$('#two').// /* Poptrox initialization disabled */
+poptrox({
+// 					caption: function($a) { return $a.next('h3').text(); },
+// 					overlayColor: '#2c2c2c',
+// 					overlayOpacity: 0.85,
+// 					popupCloserText: '',
+// 					popupLoaderText: '',
+// 					selector: '.work-item a.image',
+// 					usePopupCaption: true,
+// 					usePopupDefaultStyling: false,
+// 					usePopupEasyClose: false,
+// 					usePopupNav: true,
+// 					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+// 				});
+/* End of disabled Poptrox */
+
 
 			});
 
